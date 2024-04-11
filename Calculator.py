92d305827b656f92d040d647604ad35e615ae22d
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-04-11 17:21:21 -0400

    started working on a calculator practice I made a list on what I want to work on and got the bare minimum with the addition, subtraction, multiplication, division, and even made a function for the circle area

---
+++ Calculator.py
@@ -0,0 +1,44 @@
+import math
+"""Things to include in Calculator:
+        -arthimatic actions
+        -finding area of shapes
+        -maybe matrices, derviatives, integrals
+        -maybe being able to find x
+        -some interface with buttons if you can
+"""
+def addition(num1,num2):
+    return num1 + num2
+def subtraction(num1,num2):
+    return num1 - num2
+def multiplication(num1,num2):
+    return num1 * num2
+def division(num1,num2):
+    if num2 == 0:
+        return "Can't divide by zero"
+    else:
+        return num1 / num2
+def circle_area(radius):
+    return math.pi * math.pow(radius,2)
+def unit_circle(radian,trig):
+    #to be worked on later, trying to figure out how to do pi
+    if trig == "cos":
+        pass
+    elif trig == "sin":
+        pass
+    elif trig == "tan":
+        pass
+    elif trig == "csc":
+        pass
+    elif trig == "sec":
+        pass
+    elif trig == "cot":
+        pass
+    else:
+        return "Not valid"
+
+# print(addition(3,4))
+# print(subtraction(3,4))
+# print(multiplication(3,4))
+# print(division(3,4))
+# print(division(3,0))
+# print(circle_area(3))
