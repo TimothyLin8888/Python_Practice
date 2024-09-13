53166620fd1ef9ccb917296b686b8d9c58beeaeb
TimothyLin8888 <122694019+TimothyLin8888@users.noreply.github.com>
2024-09-13 09:34:22 -0400

    Started making Calculator code and made a Joke folder for code that are meant to be jokes

--- Calculator/Calculator.py
+++ Calculator/Calculator.py
@@ -6,6 +6,7 @@ import math
         -maybe being able to find x
         -some interface with buttons if you can
 """
+# Arthimatic Equations
 def addition(num1,num2):
     return num1 + num2
 def subtraction(num1,num2):
@@ -17,8 +18,19 @@ def division(num1,num2):
         return "Can't divide by zero"
     else:
         return num1 / num2
+    
+# Areas of Shapes
 def circle_area(radius):
     return math.pi * math.pow(radius,2)
+def square_area(num):
+    return math.pow(num,2)
+def rectangle_area(num1,num2):
+    return num1*num2
+def triangle_area(base,height):
+    return base / 2 * height
+
+
+# Trig
 def unit_circle(radian,trig):
     #to be worked on later, trying to figure out how to do pi
     if trig == "cos":
@@ -36,9 +48,17 @@ def unit_circle(radian,trig):
     else:
         return "Not valid"
 
+# finding x
+def finding_x(equation):
+    pass
+
+
+def main():
+    print("This is the main method")
 # print(addition(3,4))
 # print(subtraction(3,4))
 # print(multiplication(3,4))
 # print(division(3,4))
 # print(division(3,0))
 # print(circle_area(3))
+main()
